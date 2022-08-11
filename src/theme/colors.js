5cbefc9d002346a0bace7d6a5927ea5e35e3b37d
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2022-08-11 09:55:08 +0530

    folder structure

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+    primary: '#00000080',
+    secondary: '#AFB1B6',
+    textPrimary: '#19191B',
+    textSecondary: '#FFFFFF',
+    borderColor: '#AFB1B6',
+    backgroundColor: '#EFEFF0',
+    ratingColor: '',
+    white: '#FFFFFF',
+    black: '#000000',
+}
+
+export default colors;
