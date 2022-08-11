1758cd09af03f6a0ffab47c4394b9a22e9e5c3f9
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2022-08-11 11:46:29 +0530

    linter and prettier setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // eslint-disable-next-line react/jsx-filename-extension
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
 
 
---
+++ src/pages/SearchScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchScreen() {
+  return (
+    <div>
+        SearchScreen</div>
+  )
+}
+
+
+export default SearchScreen
