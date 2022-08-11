eb4b6cbe63abcc42019b05a7a1ab813307edaf76
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2022-08-11 11:47:33 +0530

    linter and prettier setup 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  "rules": {
-    "consistent-return": 2,
-    "indent"           : [1, 4],
-    "no-else-return"   : 1,
-    "semi"             : [1, "always"],
-    "space-unary-ops"  : 2
-}
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    "rules": {
+        "consistent-return": 2,
+        "indent"           : [1, 4],
+        "no-else-return"   : 1,
+        "semi"             : [1, "always"],
+        "space-unary-ops"  : 2
+    }
 };
